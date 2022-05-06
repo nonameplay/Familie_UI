fa3fdcf0583d9aaa98cce672580296f23edbf324
nonameplay <nonameplay17992@gmail.com>
2022-05-06 22:40:40 +0700

    update code

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -13,89 +13,100 @@ debugData([
 const App: React.FC = () => {
     return (
         <div className='flex justify-center'>
-            <div className="sm:w-3/5 sm:h-32 2xl:w-1/3 2xl:h-32 xl:w-1/3 xl:h-24 hud rounded-full absolute">
+            <div className="hud">
                 <div className="grid">
 
                     <div className="speed">
-                        <div className='flex justify-center items-end'><p className='text-4xl text-white'>100</p></div>
-                        <div><p className='text-white text-xs mb-2'>KM/H</p></div>
+                        <div className="progress-bar">
+                            <svg width="60%" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+                                <circle className="progress-bg" fill="none" stroke-width="2" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+                            </svg>
+                        </div>
+                        <div className="progress-bar">
+                            <svg width="60%" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+                                <circle className="progress" fill="none" stroke-width="2" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+                            </svg>
+                        </div>
+                        <div className='flex justify-center items-end mt-14'><p className='text-5xl text-white'>100</p></div>
+                        <div><p className='text-white text-sm'>KM/H</p></div>
+                        <div className='grid flex justify-center w-24 ml-12'>
+                            <div className='text-white -ml-6'>
+                                <p className='text-xs'>H</p>
+                            </div>
+                            <img src="https://media.discordapp.net/attachments/811222595151986710/972091432884138094/fuel.png" className='w-4 ml-3' alt="" />
+                            <div className='text-white ml-10'>
+                                <p className='text-xs'>C</p>
+                            </div>
+                            <div className="fuel_bar mt-5 -ml-2 absolute">
+                                <div className='bg-neutral-600 w-full h-1 rounded absolute'></div>
+                                <div className='bg-emerald-400 w-6/12 h-1 rounded absolute'></div>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="car-ui ml-5">
-                        <div className='border-2 border-black bg-black absolute -mt-20 p-1 -ml-64 rounded-full'>
-                            <img src="https://media.discordapp.net/attachments/811222595151986710/972082444259950612/low.png" className='w-5' alt="" />
+                        <div className='border-2 border-black bg-black absolute -mt-28 p-1 -ml-52 rounded-full'>
+                            <img src="https://media.discordapp.net/attachments/811222595151986710/972082444259950612/low.png" className='w-4' alt="" />
                         </div>
-                        <div className='border-2 border-cyan-800 bg-cyan-800 absolute -mt-20 p-1 ml-64 rounded-full'>
-                            <img src="https://media.discordapp.net/attachments/811222595151986710/972082461439823892/belt.png" className='w-5' alt="" />
+                        <div className='border-2 border-cyan-800 bg-cyan-800 absolute -mt-28 p-1 ml-40 rounded-full'>
+                            <img src="https://media.discordapp.net/attachments/811222595151986710/972082461439823892/belt.png" className='w-4' alt="" />
                         </div>
                         <div className="grid grid-rows-3 grid-flow-col gap-4 flex items-center mt-4">
-                            <div className="col-span-2 bg-zinc-800 w-12 text-xs -mt-14 ml-2 text-center text-white absolute">2.2</div>
-                            <div className="col-span-2 w-12 text-xs text-center -mt-4 -ml-1 text-white absolute">Bar</div>
-                            <div className="col-span-2 bg-zinc-800 w-12 text-xs mt-8 -ml-2 text-center text-white absolute">2.2</div>
+                            <div className="col-span-2 bg-zinc-800 w-12 text-xs -mt-8 -ml-1 pl-3 text-white absolute">2.2</div>
+                            <div className="col-span-2 w-12 text-xs text-center mt-3 -ml-4 text-white absolute">Bar</div>
+                            <div className="col-span-2 bg-zinc-800 w-8 text-xs mt-16 -ml-4 text-center text-white absolute">2.2</div>
                             <div className="row-span-3 z-40">
-                                <img src="https://cdn.discordapp.com/attachments/811222595151986710/972060069413941309/car.svg" className='w-24 ml-9' alt="" />
+                                <div className="road"></div>
+                                <img src="https://cdn.discordapp.com/attachments/811222595151986710/972060069413941309/car.svg" className='w-28 mt-7 ml-4' alt="" />
                             </div>
-                            <div className="col-span-2 bg-zinc-800 w-12 text-xs -mt-14 ml-28 text-center text-white absolute">2.2</div>
-                            <div className="col-span-2 w-12 text-xs text-center pl-4 -mt-4 ml-28 text-white absolute">Bar</div>
-                            <div className="col-span-2 bg-zinc-800 w-12 text-xs mt-8 ml-32 text-center text-white absolute">2.2</div>
+                            <div className="col-span-2 bg-zinc-800 w-12 text-xs -mt-8 ml-24 text-center pl-4 text-white absolute">2.2</div>
+                            <div className="col-span-2 w-12 text-xs text-center mt-3 ml-28 text-white absolute">Bar</div>
+                            <div className="col-span-2 bg-zinc-800 w-8 text-xs mt-16 ml-32 text-center text-white absolute">2.2</div>
                         </div>
                     </div>
 
-                    <div className="gear">
-                        <div className='flex justify-center items-end'><p className='text-4xl text-white'>2</p></div>
-                        <div className='flex justify-center items-start'>
-                            <p className='text-white text-xs mb-2'>GEAR</p>
+                    <div className="gear -ml-1">
+                        <div className="progress-bar">
+                            <svg width="100%" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+                                <circle className="progress-bg" fill="none" stroke-width="2" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+                            </svg>
                         </div>
-                    </div>
-                </div>
-                <div className='grid -mt-24'>
-                    <div className='flex justify-center items-center mt-4'>
-                        <div className='grid w-32'>
-                            <div className='flex justify-center items-center text-white'>
-                                <p className='text-xs'>F</p>
-                            </div>
-                            <div className='flex justify-center items-center'>
-                            <img src="https://media.discordapp.net/attachments/811222595151986710/972091432884138094/fuel.png" className='w-4' alt="" />
-                            </div>
-                            <div className='flex justify-center items-center text-white'>
-                                <p className='text-xs'>E</p>
-                            </div>
-                            <div className='border-b-4 border-neutral-700 w-28 rounded-lg mt-16 ml-2 absolute'></div>
-                            <div className='border-b-4 border-emerald-400 w-16 rounded-lg mt-16 ml-2 absolute'></div>
+                        <div className="progress-bar">
+                            <svg width="100%" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+                                <circle className="progress" fill="none" stroke-width="2" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+                            </svg>
                         </div>
-                    </div>
-                    <div className='flex justify-center items-center'>
-                    </div>
-                    <div className='flex justify-center items-center mt-4'>
-                        <div className='grid w-32'>
-                            <div className='flex justify-center items-center text-white'>
+                        <div className='flex justify-center items-end mt-14'><p className='text-5xl text-white'>2</p></div>
+                        <div><p className='text-white text-sm'>GEAR</p></div>
+                        <div className='grid flex justify-center w-24 ml-12'>
+                            <div className='text-white -ml-6'>
                                 <p className='text-xs'>H</p>
                             </div>
-                            <div className='flex justify-center items-center'>
-                            <img src="https://media.discordapp.net/attachments/811222595151986710/972091432645046312/tool.png" className='w-4' alt="" />
-                            </div>
-                            <div className='flex justify-center items-center text-white'>
+                            <img src="https://media.discordapp.net/attachments/811222595151986710/972091432645046312/tool.png" className='w-4 ml-3' alt="" />
+                            <div className='text-white ml-10'>
                                 <p className='text-xs'>C</p>
                             </div>
-                            <div className='border-b-4 border-neutral-700 w-28 rounded-lg mt-16 ml-2 absolute'></div>
-                            <div className='border-b-4 border-emerald-400 w-16 rounded-lg mt-16 ml-2 absolute'></div>
+                            <div className="engin_bar mt-5 -ml-2 absolute">
+                                <div className='bg-neutral-700 w-full h-1 rounded absolute'></div>
+                                <div className='bg-emerald-400 w-6/12 h-1 rounded absolute'></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-        <div className='rounded-3xl bg-neutral-800/75 text-white p-5 set-right absolute'>
-            <div className='flex justify-center items-center text-center'>
-                หากรถได้รับความเสียหาย
-                <br />
-                ส่วนที่เสียหายจะเปลี่ยนเป็นสีแดง
+            <div className='rounded-3xl bg-neutral-800/75 text-white pt-3 pb-4 set-right absolute'>
+                <div className='flex justify-center items-center text-center'>
+                    หากรถได้รับความเสียหาย
+                    <br />
+                    ส่วนที่เสียหายจะเปลี่ยนเป็นสีแดง
+                </div>
+                <div className="grid mt-5">
+                    <img src="https://cdn.discordapp.com/attachments/811222595151986710/972060069413941309/car.svg" className='w-1-12 mx-4' alt="" />
+                    <img src="https://cdn.discordapp.com/attachments/811222595151986710/972060069413941309/car.svg" className='w-1-12 mx-4' alt="" />
+                    <img src="https://cdn.discordapp.com/attachments/811222595151986710/972060069413941309/car.svg" className='w-1-12 mx-4' alt="" />
+                </div>
             </div>
-            <div className="grid mt-5">
-                <img src="https://cdn.discordapp.com/attachments/811222595151986710/972060069413941309/car.svg" className='w-20 mx-5' alt="" />
-                <img src="https://cdn.discordapp.com/attachments/811222595151986710/972060069413941309/car.svg" className='w-20 mx-5' alt="" />
-                <img src="https://cdn.discordapp.com/attachments/811222595151986710/972060069413941309/car.svg" className='w-20 mx-5' alt="" />
-            </div>
-        </div>
         </div>
     );
 }
